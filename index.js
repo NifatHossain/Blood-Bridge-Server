e1d9b7d149a0edaeebf566a421dd023231092533
NifatHossain <nifathossain1480@gmail.com>
2024-06-21 15:10:47 +0600

    Created vercel.json

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var jwt = require('jsonwebtoken');
 const port= process.env.PORT || 5000;
 require('dotenv').config()
 //middleware
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ]
+}))
 app.use(express.json());
 
 
